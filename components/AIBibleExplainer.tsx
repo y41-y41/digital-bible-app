25335e52982e6a8fdb33be612e58d2e119ec36d1
y41-y41 <y41-y41@users.noreply.github.com>
2026-03-16 00:11:23 -0400

    .

--- components/AIBibleExplainer.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// This component has been removed as per user request.
-export const AIBibleExplainer: React.FC = () => {
-    return null;
-};
--- components/SearchForm.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-// This component is no longer used in the navigation-based UI.
-// Returning null removes it from the component tree.
-export const SearchForm: React.FC<{ onSearch: (query: string) => void; isLoading: boolean; }> = () => {
-  return null;
-};
